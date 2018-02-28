7b0fa72ad9f79a5abdc774a00bc554e5eb694e6e
Princess Macanlalay <pmacanla@sc-lib-78.labs.surrey.sfu.ca>
2018-02-27 18:37:20 -0800

    testing

--- M-Brace/ViewController.swift
+++ M-Brace/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dataOutput: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func didTapButton(_ sender: UIButton) {
+        dataOutput.text = "Hello World!"
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+	
 
 }
 
