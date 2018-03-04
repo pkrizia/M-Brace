79300aedf38b80af89cc9e3a72245d7b33ffc090
Michelle <mmh12@sc-lib-77.labs.surrey.sfu.ca>
2018-03-04 14:43:54 -0800

    sample plotter (requires manual user input)

--- M-Brace/ViewController.swift
+++ M-Brace/ViewController.swift
@@ -7,50 +7,60 @@
 //
 
 import UIKit
-import SwiftSocket
+//import SwiftSocket
+import Charts
 
 class ViewController: UIViewController {
-    @IBOutlet weak var dataOutput: UITextView!
+    
+    @IBOutlet weak var txtBox: UITextField!
+    @IBOutlet weak var lnChart: LineChartView!
+    
+    var numbers : [Double] = [] //store all numbers/imported set of numbers here
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    @IBAction func didTapButton(_ sender: UIButton) {
-        var response = ""
-        let client = TCPClient(address: "127.0.0.1", port: 12000)
-        switch client.connect(timeout: 10){
-        case .success:
-            switch client.send(string: "hello world\n"){
-            case .success:
-                // timeout is necessary
-                guard let data = client.read(1024*10, timeout: 1) else {
-                    print("here")
-                    return
-                }
-                if let response = String(bytes: data, encoding: .utf8){
-                    dataOutput.text = response
-                    print(response)
-                }
-            case .failure(_):
-                response = "Error: failed to obtain response."
-                dataOutput.text = response
-                print(response)
-            }
-        case .failure(_):
-            response = "Error: failed to connect."
-            dataOutput.text = response
-            print(response)
-        }
-        client.close()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-	//TEST
+    
+    //Trigger button to plot data
+    
+    @IBAction func graphBtn(_ sender: Any) {
+        let input = Double(txtBox.text!)
+        numbers.append(input!)
+        updateGraph()
+    }
+    
+//
+//    @IBAction func graphBtn(_ sender: Any) {
+//        let input = Double(txtBox.text!) //input from user
+//        numbers.append(input!)
+//        updateGraph()
+//    }
+    
+    func updateGraph(){
+        var lnChartData = [ChartDataEntry]() //this is the array of points that will be displayed on graph
+        
+        for i in 0..<numbers.count{
+            let value = ChartDataEntry(x: Double(i), y: numbers[i])
+            lnChartData.append(value)
+        }
+        let line1 = LineChartDataSet(values: lnChartData, label: "Numbers") //convert data points into a dataset
+        
+        line1.colors = [NSUIColor.white]
+        
+        let data = LineChartData() //object will be added to chart
+        
+        data.addDataSet(line1)
+        
+        lnChart.data = data //adds the chart data to chart and graph updates
+        lnChart.chartDescription?.text = "line graph"
+    }
+    
+
 
 }
 
