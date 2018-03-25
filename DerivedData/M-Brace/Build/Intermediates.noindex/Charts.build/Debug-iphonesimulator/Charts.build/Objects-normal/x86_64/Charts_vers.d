4d5e2b68ab5fd9d80d460788e3dcafcdae3da92a
meisoh <michellemho@yahoo.ca>
2018-03-25 16:53:00 -0700

    trying to commit/push multi-lines

---
+++ DerivedData/M-Brace/Build/Intermediates.noindex/Charts.build/Debug-iphonesimulator/Charts.build/Objects-normal/x86_64/Charts_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/MichelleH/Desktop/CAPSTONE/M-Brace/DerivedData/M-Brace/Build/Intermediates.noindex/Charts.build/Debug-iphonesimulator/Charts.build/DerivedSources/Charts_vers.c
---
+++ DerivedData/M-Brace/Build/Intermediates.noindex/SwiftSocket.build/Debug-iphonesimulator/SwiftSocket iOS.build/Objects-normal/x86_64/yudpsocket.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/MichelleH/Desktop/CAPSTONE/M-Brace/Pods/SwiftSocket-master/Sources/yudpsocket.c \
+  /Users/MichelleH/Desktop/CAPSTONE/M-Brace/DerivedData/M-Brace/Build/Intermediates.noindex/SwiftSocket.build/Debug-iphonesimulator/SwiftSocket\ iOS.build/module.modulemap
