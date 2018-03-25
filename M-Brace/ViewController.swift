50c6f9ce5ff35b3f7b0abd67271ae73182b69e5f
meisoh <michellemho@yahoo.ca>
2018-03-25 16:49:31 -0700

    Added another line

--- M-Brace/ViewController.swift
+++ M-Brace/ViewController.swift
@@ -14,9 +14,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var txtBox: UITextField!
     @IBOutlet weak var lnChart: LineChartView!
+    @IBOutlet weak var txtBox2: UITextField!
     
     var numbers : [Double] = [] //store all numbers/imported set of numbers here
-    
+    var numbers2: [Double] = []
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -31,9 +32,11 @@ class ViewController: UIViewController {
     @IBAction func graphBtn(_ sender: Any) {
         let input = Double(txtBox.text!)
         numbers.append(input!)
+        let input2 = Double(txtBox2.text!)
+        numbers2.append(input2!)
         updateGraph()
     }
-    
+    //make 2 diff lines or multiple (one for each sensor)
 //
 //    @IBAction func graphBtn(_ sender: Any) {
 //        let input = Double(txtBox.text!) //input from user
@@ -43,18 +46,28 @@ class ViewController: UIViewController {
     
     func updateGraph(){
         var lnChartData = [ChartDataEntry]() //this is the array of points that will be displayed on graph
+        var lnChartData2 = [ChartDataEntry]()
         
         for i in 0..<numbers.count{
             let value = ChartDataEntry(x: Double(i), y: numbers[i])
             lnChartData.append(value)
+            let value2 = ChartDataEntry(x: Double(i), y: numbers2[i])
+            lnChartData2.append(value2)
+            //lnChartData2 = lnChartData + lnChartData2
         }
-        let line1 = LineChartDataSet(values: lnChartData, label: "Numbers") //convert data points into a dataset
+//        let line1 = LineChartDataSet(values: lnChartData, label: "Numbers") //convert data points into a dataset
+//        line1.colors = [NSUIColor.white]
+//        let data = LineChartData() //object will be added to chart
+//        data.addDataSet(line1)
+//
+        let line1 = LineChartDataSet(values: lnChartData, label: "Line1") //convert data points into a dataset
+        line1.setColor(.blue)
         
-        line1.colors = [NSUIColor.white]
+        let line2 = LineChartDataSet(values: lnChartData2, label: "Line2")
+        line2.setColor(.red)
         
-        let data = LineChartData() //object will be added to chart
-        
-        data.addDataSet(line1)
+        let data = LineChartData(dataSets: [line1,line2]) //object will be added to chart
+        //data.addDataSet(line1, line2)
         
         lnChart.data = data //adds the chart data to chart and graph updates
         lnChart.chartDescription?.text = "line graph"
