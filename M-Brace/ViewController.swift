fa10f17f86fac964a4672a59fa523c4a8e9ae337
pkrizia <princess.macanlalay@gmail.com>
2018-03-29 18:07:49 -0700

    working sensor chart data class

--- M-Brace/ViewController.swift
+++ M-Brace/ViewController.swift
@@ -16,16 +16,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var lnChart: LineChartView!
     @IBOutlet weak var txtBox2: UITextField!
     
-    struct GraphData {
-        var numDataSets : Int
+    class SensorData {
+        var numSensors : Int
         var data = [[Double]]() //store all numbers/imported set of numbers here
-        //var numbers2: [Double] = [] //added another array for 2nd sensor
+        let colorArray: [UIColor] = [.blue, .red, .magenta, .green, .brown, .orange]
+        let labelArray: [String] = ["Line 1", "Line 2", "Line 3", "Line 4", "Line 5", "Line 6"]
         
-        init(numVal: Int) {
-            self.numDataSets = numVal
-            for _ in 0...self.numDataSets{
-                self.data.append([0])
+        init(numSensors: Int) {
+            self.numSensors = numSensors
+            for _ in 0...self.numSensors{
+                self.data.append([])
             }
+        }
+        
+        func getSensorData(sensorValues: [Double]) {
+            for i in 0..<self.numSensors {
+                self.data[i].append(sensorValues[i])
+                if (self.data[i].count > 15) {
+                    self.data[i].removeFirst()
+                }
+            }
+        }
+        
+        func createSensorChartData() -> (LineChartData) {
+            var lnChartData = [[ChartDataEntry]]() //array of points that will be displayed on graph
+            var line: [IChartDataSet] = []
+            
+            for _ in 0...self.numSensors {
+                lnChartData.append([])
+            }
+            
+            for j in 0..<self.numSensors {
+                for i in 0..<self.data[0].count {
+                    let value = ChartDataEntry(x: Double(i), y: self.data[j][i])
+                    lnChartData[j].append(value)
+                }
+                line.append(LineChartDataSet(values: lnChartData[j], label: self.labelArray[j]))
+                line[j].setColor(self.colorArray[j])
+            }
+            return LineChartData(dataSets: line) //object will be added to chart
         }
     }
     
@@ -39,7 +68,7 @@ class ViewController: UIViewController {
     }
     
     //Trigger button to plot data
-    var numbers = GraphData(numVal: 2)
+    var numbers = SensorData(numSensors: 2)
     @IBAction func graphBtn(_ sender: Any) {
         let input = Double(txtBox.text!)
         numbers.data[0].append(input!)
@@ -51,7 +80,7 @@ class ViewController: UIViewController {
         if (numbers.data[1].count > 15) {
             numbers.data[1].removeFirst()
         }
-        updateGraph(value: numbers.data)
+        updateGraph(numbers: numbers)
     }
     
     @IBAction func didTapButton(_ sender: UIButton) {
@@ -83,33 +112,35 @@ class ViewController: UIViewController {
         client.close()
     }
     
-    func updateGraph(value: [[Double]]){
-        var lnChartData = [ChartDataEntry]() //this is the array of points that will be displayed on graph
-        var lnChartData2 = [ChartDataEntry]()
+    func updateGraph(numbers: SensorData){
+        let data = numbers.createSensorChartData()
+        lnChart.data = data //adds the chart data to chart and graph updates
+        lnChart.chartDescription?.text = "line graph"
+        /*
+        var lnChartData = [[ChartDataEntry]]() //this is the array of points that will be displayed on graph
+        var line: [IChartDataSet] = []
+        
+        for _ in 0...numbers.numSensors {
+            lnChartData.append([])
+        }
         
         for i in 0..<numbers.data[0].count{
             let value = ChartDataEntry(x: Double(i), y: numbers.data[0][i])
-            lnChartData.append(value)
+            lnChartData[0].append(value)
             let value2 = ChartDataEntry(x: Double(i), y: numbers.data[1][i])
-            lnChartData2.append(value2)
-            //lnChartData2 = lnChartData + lnChartData2
+            lnChartData[1].append(value2)
         }
-//        let line1 = LineChartDataSet(values: lnChartData, label: "Numbers") //convert data points into a dataset
-//        line1.colors = [NSUIColor.white]
-//        let data = LineChartData() //object will be added to chart
-//        data.addDataSet(line1)
-//
-        let line1 = LineChartDataSet(values: lnChartData, label: "Line1") //convert data points into a dataset
-        line1.setColor(.blue)
+        line.append(LineChartDataSet(values: lnChartData[0], label: "Line1"))
+        line[0].setColor(.blue)
         
-        let line2 = LineChartDataSet(values: lnChartData2, label: "Line2")
-        line2.setColor(.red)
+        line.append(LineChartDataSet(values: lnChartData[1], label: "Line2"))
+        line[1].setColor(.red)
         
-        let data = LineChartData(dataSets: [line1,line2]) //object will be added to chart
-        //data.addDataSet(line1, line2)
+        let data = LineChartData(dataSets: line) //object will be added to chart
+    */
         
-        lnChart.data = data //adds the chart data to chart and graph updates
-        lnChart.chartDescription?.text = "line graph"
+        //lnChart.data = data //adds the chart data to chart and graph updates
+        //lnChart.chartDescription?.text = "line graph"
     }
     
 
